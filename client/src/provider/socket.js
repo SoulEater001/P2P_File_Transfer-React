9089fa1952f84e41fe4c440d049b62134e1d97e2
SoulEater001 <dshivam698@gmail.com>
2025-01-24 13:06:28 +0530

    updated urls

--- client/src/provider/socket.js
+++ client/src/provider/socket.js
@@ -2,7 +2,7 @@
 import { io } from 'socket.io-client';
 
 // Create the socket instance and export it
-const socket = io('http://localhost:5000', {
+const socket = io('https://p2pfiletransfer-react-backend-production.up.railway.app', {
     transports: ['websocket', 'polling'],
 });
 
